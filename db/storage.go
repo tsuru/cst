077763c73fc22ee90401fc3475f6dd0d1f80b6ea
Claudio Netto <nettinhorama@gmail.com>
2018-07-10 15:09:13 -0300

    db: create a singleton instance of Storage

--- db/storage.go
+++ db/storage.go
@@ -7,3 +7,17 @@ type Storage interface {
 	Close()
 	Save(scan.Scan) error
 }
+
+var storageInstance Storage
+
+// SetStorage sets the storage parameter to be used globally by any that calls
+// GetStorage function.
+func SetStorage(storage Storage) {
+	storageInstance = storage
+}
+
+// GetStorage returns the current storage instance. Make sure you set a storage
+// instance (calling the SetStorage function) before.
+func GetStorage() Storage {
+	return storageInstance
+}
