57298b48e5b5b23d249704b68f2488c13d4831f7
Claudio Netto <nettinhorama@gmail.com>
2018-07-11 11:53:50 -0300

    cmd/server: add arg to get database url connection

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -8,12 +8,18 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/tsuru/cst/api"
+	"github.com/tsuru/cst/db"
+	"github.com/tsuru/cst/db/mongodb"
+	"github.com/tsuru/cst/queue"
 )
 
 var (
 	webserver api.WebServer
 
 	signalChan = make(chan os.Signal, 1)
+
+	newQueue   = queue.NewQueue
+	newStorage = mongodb.NewMongoDB
 )
 
 // New creates an instance of server command.
@@ -35,17 +41,40 @@ func New() *cobra.Command {
 	serverCmd.Flags().
 		IntP("port", "p", 8443, "port to listen")
 
+	serverCmd.Flags().
+		String("database", "", "database URL connection (required)")
+
 	serverCmd.MarkFlagRequired("cert-file")
 	serverCmd.MarkFlagRequired("key-file")
+	serverCmd.MarkFlagRequired("database")
 
 	viper.BindPFlag("server.cert-file", serverCmd.Flags().Lookup("cert-file"))
 	viper.BindPFlag("server.key-file", serverCmd.Flags().Lookup("key-file"))
 	viper.BindPFlag("server.port", serverCmd.Flags().Lookup("port"))
+	viper.BindPFlag("server.database", serverCmd.Flags().Lookup("database"))
 
 	return serverCmd
 }
 
 func serverCommandPreRun(cmd *cobra.Command, args []string) {
+
+	databaseURL := viper.GetString("server.database")
+
+	database, err := newStorage(databaseURL)
+
+	if err != nil {
+		logrus.WithError(err).Fatal("problem to connect on storage service")
+	}
+
+	db.SetStorage(database)
+
+	q, err := newQueue(databaseURL)
+
+	if err != nil {
+		logrus.WithError(err).Fatal("problem to connect on queue service")
+	}
+
+	queue.SetQueue(q)
 
 	webserver = &api.SecureWebServer{
 		CertFile: viper.GetString("server.cert-file"),
