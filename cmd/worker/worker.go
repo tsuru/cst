3e200e6043d3152fc73c3784fd9b9af946cffd88
Claudio Netto <nettinhorama@gmail.com>
2018-07-12 15:27:47 -0300

    cmd/worker: get clair address from cli and register it for analyze imgs

--- cmd/worker/worker.go
+++ cmd/worker/worker.go
@@ -3,6 +3,9 @@ package worker
 import (
 	"os"
 	"os/signal"
+	"time"
+
+	"github.com/tsuru/monsterqueue"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -10,9 +13,13 @@ import (
 	"github.com/tsuru/cst/db"
 	"github.com/tsuru/cst/db/mongodb"
 	"github.com/tsuru/cst/queue"
+	"github.com/tsuru/cst/scan"
+	"github.com/tsuru/cst/scan/worker"
 )
 
 var (
+	scanTask monsterqueue.Task
+
 	signalChan = make(chan os.Signal, 1)
 
 	newQueue   = queue.NewQueue
@@ -24,7 +31,7 @@ func New() *cobra.Command {
 
 	workerCmd := &cobra.Command{
 		Use:    "worker",
-		Short:  "Run worker to fire on scheduled scans",
+		Short:  "Run worker to analyze scheduled scans",
 		PreRun: workerCommandPreRun,
 		Run:    workerCommandRun,
 	}
@@ -32,9 +39,14 @@ func New() *cobra.Command {
 	workerCmd.Flags().
 		String("database", "", "database URL connection (required)")
 
+	workerCmd.Flags().
+		String("clair-address", "", "CoresOS Clair address (required)")
+
 	workerCmd.MarkFlagRequired("database")
+	workerCmd.MarkFlagRequired("clair-address")
 
 	viper.BindPFlag("worker.database", workerCmd.Flags().Lookup("database"))
+	viper.BindPFlag("worker.clair.address", workerCmd.Flags().Lookup("clair-address"))
 
 	return workerCmd
 }
@@ -58,11 +70,25 @@ func workerCommandPreRun(cmd *cobra.Command, args []string) {
 	}
 
 	queue.SetQueue(q)
+
+	clair := &scan.Clair{
+		Address: viper.GetString("worker.clair.address"),
+		Name:    "clair",
+		Timeout: time.Minute,
+	}
+
+	scanTask = &worker.ScanTask{
+		Scanners: []scan.Scanner{
+			clair,
+		},
+	}
 }
 
 func workerCommandRun(cmd *cobra.Command, args []string) {
 
 	q := queue.GetQueue()
+
+	q.RegisterTask(scanTask)
 
 	// process the jobs in another thread to be able to handle signals
 	go q.ProcessLoop()
--- cmd/worker/worker_test.go
+++ cmd/worker/worker_test.go
@@ -1,10 +1,12 @@
 package worker
 
 import (
+	"bytes"
 	"os"
 	"testing"
 	"time"
 
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"github.com/tsuru/cst/db"
@@ -76,5 +78,55 @@ func TestWorkerCommandRun(t *testing.T) {
 		time.Sleep(time.Second)
 
 		assert.True(t, hasCalledStorageClose)
+	})
+}
+
+func TestNew(t *testing.T) {
+
+	t.Run(`When required args are not assigned, should retuns a error`, func(t *testing.T) {
+
+		errorArgs := [][]string{
+			[]string{},
+			[]string{
+				"--database", "mongodb://localhost:27018/mydb",
+			},
+			[]string{
+				"--clair-address", "https://clair.tld:6060",
+			},
+		}
+
+		for _, args := range errorArgs {
+			workerCmd := New()
+
+			workerCmd.PreRun = nil
+			workerCmd.Run = func(cmd *cobra.Command, args []string) {}
+
+			workerCmd.SetOutput(bytes.NewBufferString(""))
+			workerCmd.SetArgs(args)
+
+			assert.Error(t, workerCmd.Execute())
+		}
+	})
+
+	t.Run(`When all required parameters are defined, should returns no errors`, func(t *testing.T) {
+
+		successfulArgs := [][]string{
+			[]string{
+				"--database", "mongodb://127.0.0.1:27017/",
+				"--clair-address", "https://clair.top.level.domain:8443",
+			},
+		}
+
+		for _, args := range successfulArgs {
+			workerCmd := New()
+
+			workerCmd.PreRun = nil
+			workerCmd.Run = func(cmd *cobra.Command, args []string) {}
+
+			workerCmd.SetOutput(bytes.NewBufferString(""))
+			workerCmd.SetArgs(args)
+
+			assert.NoError(t, workerCmd.Execute())
+		}
 	})
 }
